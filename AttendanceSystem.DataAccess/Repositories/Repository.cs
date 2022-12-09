35f918b9de1f45d84e57bd5019a776e61300545a
hossein karimy <hosseinkm01@gmail.com>
2022-12-10 02:47:38 +0330

    Update Reposiotory And Teacher Repo

--- AttendanceSystem.DataAccess/Repositories/Repository.cs
+++ AttendanceSystem.DataAccess/Repositories/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using AttendanceSystem.DataAccess.Data;
 using AttendanceSystem.DataAccess.Repositories.IRepositories;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace AttendanceSystem.DataAccess.Repositories;
 
@@ -22,13 +23,29 @@ public class Repository<T> : IRepository<T> where T : class
         DbSet.Remove(entity);
     }
 
+    public T FirstOrDefault(Expression<Func<T, bool>> expression, string includes)
+    {
+        return DbSet.Include(includes).FirstOrDefault(expression);
+    }
+
+    public T FirstOrDefault(Expression<Func<T, bool>> expression)
+    {
+        return DbSet.FirstOrDefault(expression);
+    }
+
     public List<T> GetAll()
     {
         return DbSet.ToList();
+    }
+
+    public List<T> GetAll(string include)
+    {
+        return DbSet.Include(include).ToList();
     }
 
     public void Update(T entity)
     {
         DbSet.Update(entity);
     }
+
 }
--- AttendanceSystem.DataAccess/Repositories/TeacherRepository.cs
+++ AttendanceSystem.DataAccess/Repositories/TeacherRepository.cs
@@ -1,12 +1,23 @@
 ﻿using AttendanceSystem.DataAccess.Data;
 using AttendanceSystem.DataAccess.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
 using Models.Models;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AttendanceSystem.DataAccess.Repositories;
 
 public class TeacherRepository : Repository<TeacherModel>, ITeacherRepository
 {
+    private readonly AppDbContext dbContext;
+
     public TeacherRepository(AppDbContext dbContext) : base(dbContext)
     {
+        this.dbContext = dbContext;
+    }
+
+    public List<TeacherModel> GetAllTeachersWithIncludes()
+    {
+        return dbContext.Teachers.Include(u => u.Courses).ThenInclude(c => c.Sections).ThenInclude(s=>s.StudentsStatus).ToList();
     }
 }
